dadd370d36ea22707ebb03623dac5f86871464af
Khushi Rana <91715072+KhushiRana2003@users.noreply.github.com>
2023-06-07 18:52:01 +0530

    Create Day 3: Power of Power.cpp

---
+++ Day 3: Power of Power.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+
+#define ll long long
+
+ll fastPower(ll a, ll b, ll n)
+
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if ((b & 1) != 0)
+        {
+            res = (res * a % n) % n;
+        }
+
+        a = (a % n * a % n) % n;
+        b = b >> 1;
+    }
+    return res;
+}
+
+int powerOfPower(int A, int B, int C, int M) 
+
+{
+    // Write your code here.
+   int res=fastPower(B,C,M-1);
+
+   res=fastPower(A,res,M);
+
+   return res;
+}
